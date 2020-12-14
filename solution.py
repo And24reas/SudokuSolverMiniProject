e993c9014ef5a0d53c9faf4ad08717f3f4171a8b
andreas <meinakkount@gmail.com>
2020-12-14 20:03:09 +0100

    progress

--- solution.py
+++ solution.py
@@ -8,12 +8,62 @@ puzzle = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
           [0, 0, 0, 0, 0, 0, 0, 7, 4],
           [0, 0, 5, 2, 0, 6, 3, 0, 0]]
 
+#print puzzle in console
 def print_puzzle(puzzle):
+    print()
     for i in range(len(puzzle)):
+        if i%3 == 0 and i!=0:
+            print("------------------------")
         for j in range(len(puzzle[i])):
+            if j%3 == 0 and j!=0:
+                print(" | ", end="")
             if j%8 == 0 and j!=0:
                 print(puzzle[i][j])
             else:
-                print(puzzle[i][j], end="")
+                print(str(puzzle[i][j]) + " ", end="")
+
 
 print_puzzle(puzzle)
+empty_positions=[]
+"""
+Order of Execution:
+    - Find empty position
+    - test an input for validity starting from 1
+    - check if that input already exists in that row
+    - check if that input already exists in that column
+    - check if it already exists in given 3x3 box
+    - if not assign that input to the empty position
+    - if it already exists try it's succesor
+    - if there's no more available successor then we take a 
+      step back to the previous cell and try the next following value
+    - repeat this process until solved
+"""
+def search_for_zero(puzzle):
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle[i])):
+            if puzzle[i][j]==0:
+                return i,j
+    return True
+
+def solve(puzzle):
+    cell = search_for_zero(puzzle)
+    
+    if cell:
+        return True
+    
+    else:
+        row, col = cell
+
+    for i in range(len(puzzle)):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+
+
