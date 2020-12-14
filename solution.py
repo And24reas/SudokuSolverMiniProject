a59edd47acce9f0b4266b9bb6868dc46fb1502f9
andreas <meinakkount@gmail.com>
2020-12-14 17:43:36 +0100

    initial commit

---
+++ solution.py
@@ -0,0 +1,19 @@
+puzzle = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
+          [5, 2, 0, 0, 0, 0, 0, 0, 0],
+          [0, 8, 7, 0, 0, 0, 0, 3, 1],
+          [0, 0, 3, 0, 1, 0, 0, 8, 0],
+          [9, 0, 0, 8, 6, 3, 0, 0, 5],
+          [0, 5, 0, 0, 9, 0, 6, 0, 0],
+          [1, 3, 0, 0, 0, 0, 2, 5, 0],
+          [0, 0, 0, 0, 0, 0, 0, 7, 4],
+          [0, 0, 5, 2, 0, 6, 3, 0, 0]]
+
+def print_puzzle(puzzle):
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle[i])):
+            if j%8 == 0 and j!=0:
+                print(puzzle[i][j])
+            else:
+                print(puzzle[i][j], end="")
+
+print_puzzle(puzzle)
